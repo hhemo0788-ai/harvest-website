b941a65989a069a4134baf0845866d404f0c7214
ibrahim magdy <hhemo0788@gmail.com>
2026-02-05 19:33:28 +0200

    Fix server for Railway

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 const db = require('./database');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Configure Multer for Image Uploads
 const storage = multer.diskStorage({
@@ -170,6 +170,4 @@ app.get('/api/last-updated', (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
