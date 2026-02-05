110e0fd410cc04f5e076f01b31e5f287d9769485
ibrahim magdy <hhemo0788@gmail.com>
2026-02-05 19:42:07 +0200

    Replace bcrypt with bcryptjs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const multer = require('multer');
 const db = require('./database');
 
